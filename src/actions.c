4c5ecdb7d55da643c72f105bdc31aed0867936c3
Lucas DEBOUTÉ <P147x.x@gmail.com>
2017-03-17 11:54:12 +0100

    sourri

--- src/actions.c
+++ src/actions.c
@@ -5,7 +5,7 @@
 ** Login   <lucas.deboute@epitech.eu>
 ** 
 ** Started on  Sat Mar 11 14:27:21 2017 Lucas Debouté
-** Last update Fri Mar 17 11:13:05 2017 Lucas Debouté
+** Last update Fri Mar 17 11:51:01 2017 Lucas Debouté
 */
 
 #include "philosophers.h"
@@ -17,11 +17,11 @@ void	action_eat(t_philo *philo)
   lphilo_eat();
   philo->action = EAT;
   philo->bowl -= 1;
-  usleep(30);
   pthread_mutex_unlock(&philo->chopsticks);
   pthread_mutex_unlock(&philo->neighbor->chopsticks);
   lphilo_release_chopstick(&philo->chopsticks);
   lphilo_release_chopstick(&philo->neighbor->chopsticks);
+  usleep(100);
 }
 
 void	action_think(t_philo *philo)
@@ -31,11 +31,12 @@ void	action_think(t_philo *philo)
   philo->action = THINK;
   pthread_mutex_unlock(&philo->chopsticks);
   lphilo_release_chopstick(&philo->chopsticks);
+  usleep(100);
 }
 
 void	action_rest(t_philo *philo)
 {
   lphilo_sleep();
-  usleep(30);
   philo->action = REST;
+  usleep(100);
 }
